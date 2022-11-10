b77edb5ebdbe126526815620de58d2ed09c4b970
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-10 16:51:11 -0400

    Add Sover FIzzBuzz

--- src/solver.rb
+++ src/solver.rb
@@ -8,4 +8,15 @@ class Solver
     return s if s.length <= 1
     return reverse(s[1..-1]) + s[0]
   end
+
+	def fizzBuzz(number)
+		rtn_str = ""
+		if number % 3 === 0
+			rtn_str += 'Fizz'
+		end
+		if number % 5 === 0
+			rtn_str += 'Buzz'
+		end
+		rtn_str.empty? ? number : rtn_str
+	end
 end
