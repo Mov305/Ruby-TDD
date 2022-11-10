a272f99aa68b96bc6d3b794345002ff4d5b6180d
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-10 16:42:58 -0400

    Add test for FizzBuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -20,4 +20,24 @@ describe Solver do
       expect(@solver.reverse("hello")).to eq("olleh")
     end
   end
+	describe "#FizzBizz" do
+		it "Numbers divisible by only 3 should return 'Fizz'" do
+			expect(@solver.fizzBuzz(13*3)).to eq('Fizz')
+		end
+
+		it "Numbers divisible by only 5 should return 'Buzz'" do
+			expect(@solver.fizzBuzz(13*5)).to eq('Buzz')
+		end
+
+		it "Numbers divisible by both 3 and 5 should return 'FizzBuzz'" do
+			expect(@solver.fizzBuzz(13*3*5)).to eq('FizzBuzz')
+		end
+
+		it "Numbers not divisible by either 3 or 5 should return the number itself" do
+			expect(@solver.fizzBuzz(13)).to eq(13)
+		end
+
+
+	end
+
 end
